d47c085a1c211fe86fdb55524f0f9d7aae3f1a77
Zixiang Hu <144851646+ZackHu-2001@users.noreply.github.com>
2025-09-09 11:16:10 -0700

    feat: support swagger under prod

--- ll-lamb-backend/ll-lamb-backend/Program.cs
+++ ll-lamb-backend/ll-lamb-backend/Program.cs
@@ -93,11 +93,8 @@ builder.Services.AddScoped<IAzureOpenAIService, AzureOpenAIService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
 
 app.UseHttpsRedirection();
 app.UseCors("AllowAllOrigins");
